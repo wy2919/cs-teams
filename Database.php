a5bc5cf89888279d7307d9096ab7d70f888032e1
intensivee <mrorange016@gmail.com>
2021-01-26 18:10:44 +0100

    Project is now prepared for deploy on Heroku;

--- Database.php
+++ Database.php
@@ -11,10 +11,10 @@ class Database
 
     public function __construct()
     {
-        $this->username = USERNAME;
-        $this->password = PASSWORD;
-        $this->host = HOST;
-        $this->database = DATABASE;
+        $this->username = getenv('USERNAME');
+        $this->password = getenv('PASSWORD');
+        $this->host = getenv('HOST');
+        $this->database = getenv('DATABASE');
     }
 
     public function connect()
